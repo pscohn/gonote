26e324e1b4f2fba176e841c696a774a599073a6d
Paul Cohn <pscohn@gmail.com>
2015-02-24 18:38:28 -0500

    Initial commit

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+
+	"github.com/pscohn/gonote/models"
+)
+
+var (
+	newNote     = flag.Bool("n", false, "save a new note")
+	newCategory = flag.Bool("c", false, "create a category")
+	get         = flag.Bool("g", false, "get recent notes")
+	dest        = flag.String("d", "", "destination for note")
+	db          models.Database
+)
+
+func runCmd(arg string) {
+	fmt.Println("destination:", *dest)
+	if *dest == "" {
+		//TODO: handle error better
+		return
+	}
+	if *newNote {
+		note := models.Note{Id: 0, Note: arg}
+		db.DB.Save(&note)
+		fmt.Printf("added to %v: \"%v\"\n", *dest, arg)
+	} else if *newCategory {
+		category := models.Category{Name: arg}
+		db.DB.Save(&category)
+		fmt.Printf("created category \"%v\"", arg)
+	} else if *get {
+		fmt.Println("get note:", arg)
+	}
+}
+
+func main() {
+	_ = db.Setup()
+	defer db.Close()
+	flag.Parse()
+	arg := strings.Join(flag.Args(), " ")
+	runCmd(arg)
+}
---
+++ models/models.go
@@ -0,0 +1,50 @@
+package models
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/mattn/go-sqlite3"
+	"os"
+)
+
+var dbName = "./com.db"
+
+type Category struct {
+	Id   int64
+	Name string `sql:"size:255"`
+}
+
+type Note struct {
+	Id         int64
+	CategoryID int64
+	Note       string `sql:"size:1024"`
+}
+
+type Database struct {
+	DB gorm.DB
+}
+
+func (d *Database) Connect() error {
+	var err error
+	d.DB, err = gorm.Open("sqlite3", dbName)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (d *Database) Setup() error {
+	if _, err := os.Stat(dbName); os.IsNotExist(err) {
+		if err := d.Connect(); err != nil {
+			return err
+		}
+
+		d.DB.DB()
+		d.DB.CreateTable(&Category{})
+		d.DB.CreateTable(&Note{})
+	}
+	return nil
+}
+
+func (d *Database) Close() {
+	d.DB.Close()
+}
